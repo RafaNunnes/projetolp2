7a0ec9d6a66882ecda2966989dc68289885eef9e
Gabriel "mizera" Fleig Alves <fleigalves@hotmail.com>
2016-11-28 15:37:42 -0300

    Add files via upload
    Servidor do taxi. Instruções no TaxiServer

---
+++ Servidor Taxi/src/ProdutorConsumidorBufferCircular/TaxiGenerator.java
@@ -0,0 +1,24 @@
+package ProdutorConsumidorBufferCircular;
+
+public class TaxiGenerator extends Thread{
+
+	TaxiBuffer buffer;
+	String value;
+	
+	public TaxiGenerator(TaxiBuffer b, String v)
+	{
+		buffer = b;
+		value = v;
+	}
+	
+	public void run()
+	{		
+		while(true)
+		{
+			buffer.deposit();						// tinha value + String.valueOf(i++) dentro do deposit			
+			try {		
+				Thread.sleep(5000);					//espera 5 segundos para depositar novo taxi					
+			} catch (InterruptedException e) {}
+		}
+	}
+}
